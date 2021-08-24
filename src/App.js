18001c58664af53a884bbaac03ee2622199e3f51
Lisandro <lisandro_luna@ymail.com>
2021-08-24 19:33:27 -0300

    mensaje de bienvenida

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenido a Coder! 
+          Bienvenido a Coder camada 19735! 
         </p>
         <a
           className="App-link"
